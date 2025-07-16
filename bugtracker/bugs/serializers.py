9567e4114b3eacd23bf8b5221864d8f88abc52ae
amitkumar_0128 <jangidamitkumar49@gmail.com>
2025-07-16 19:28:48 +0530

    refactor: directory structure change

--- bugtracker/bugs/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import BugReport
-
-class BugReportSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BugReport
-        fields = '__all__'
